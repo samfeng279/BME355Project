3eedf41316021f8750080cc303e0f20a86eb32c7
Unknown <sawillerusa@gmail.com>
2018-04-02 00:46:31 -0400

    Integrated activation code
    not currently calling it in model

--- SwingPhaseModel.m
+++ SwingPhaseModel.m
@@ -15,7 +15,13 @@ classdef SwingPhaseModel < handle
         shankLength = 0.4318; 
         ankleFootLength = 0.07136; % Distance from ankle to COM of foot
         duration = 0.6; % Duration of simulation
-        fps = 100; % Frames per sec - changes resolution of animation  
+        fps = 100; % Frames per sec - changes resolution of animation 
+        modelledTA; 
+        modelledS;
+        %need to find the correct values for these
+        gaitInterval = 2;
+        swingInterval = 0;
+        pointNum = 5000;
     end
     
     methods (Static)
@@ -58,6 +64,230 @@ classdef SwingPhaseModel < handle
             if size(theta, 1) > size(theta, 2)
                 result = result';
             end
+        end 
+        
+        % function for running regression on the muscle activation graph to
+        % get modellig equations
+        function result = getActivationRegression(data, gaitInterval)
+            % updating time values to fit with the gait interval
+            for i = 1:size(data,1)
+                data(i,1) = data(i,1)*(10^-2)*gaitInterval;
+            end
+
+            % finding the regressed line for each point to map the curve
+            delta = 5;
+            regressions = []; % matrix of time points and their corresponding coeffs
+            for i = 1:size(data,1)
+                % finding start and end points of data interval being regressed
+                if i < delta + 1
+                    s = 1;
+                    e = i + delta;
+                elseif (size(data,1) - i) < delta
+                    s = i - delta;
+                    e = size(data,1);
+                else
+                    s = i - delta;
+                    e = i + delta;
+                end
+
+                %take care of a zero case, otherwise we only begin when t > 0 
+                if i == 1
+                    x = [0; data(1:9,1)];
+                    y = [data(1,2); data(1:9,2)];
+                    coeffs = polyfit(x,y,2);
+                    regressions = [regressions; 0 coeffs(1) coeffs(2) coeffs(3)];
+                end
+
+                x = data(s:e,1);
+                y = data(s:e,2);
+                coeffs = polyfit(x,y,2);
+                regressions = [regressions; data(i,1) coeffs(1) coeffs(2) coeffs(3)];
+            end
+            
+            result = regressions;            
+        end
+        
+        % fuction for obtaining simulated activation values from modelled
+        % curve
+        function result = getSimulatedActivations(regressions, gaitInterval, swingInterval)
+            % data set gives points to model the activation of TA, and from
+            % here we calculate activations of TA through the gait cycle
+            % using the regression -> create new time array (select point #)
+            times = linspace(0, gaitInterval, 5000);
+            simulated = [];
+            for i = 1:size(times,2)
+                for j = 1:size(regressions,1)-1
+                    % finding which interval of the regression data the new time falls into
+                    if (times(i) >= regressions(j,1)) && (times(i) <= regressions(j+1,1))
+                        % now that we've found the interval, we can approx the
+                        % activation value using either j's or j+1's polynomial
+                        a = polyval([regressions(j, 2) regressions(j, 3) regressions(j, 4)], times(i));
+                        simulated = [simulated; times(i) a];
+                        break;
+                    end
+                end
+            end
+            
+            % TO DO: clip data for the swing phase using swingInterval
+            % (will also need to alter code in below function)
+            
+            result = simulated;
+        end
+        
+        %20-50Hz
+        % will be loading data = csvread('TA_STIM.csv'); 
+        function SP = getModelledTA(SP, data, frequency)
+            regressions = getActivationRegression(data, SP.gaitInterval);
+            simulatedTA = getSimulatedActivations(regressions, SP.gaitInterval, SP.swingInterval);
+            
+            % using frequency to find how many points exist between zeros and peaks
+            
+            delta = (SP.pointNum/SP.gaitInterval)/frequency;
+            % identifying where peaks and zeros occur, as well as identifying the linear
+            % relation between adjacents
+            latestPeak = 1; latestZero = 1;
+            peaks = []; zeros = [1];
+            eqns = []; % store info in format: start_index end_index coeff(1) coeff(2)
+            for i = 1:size(simulatedTA,1)    
+                % identify where and when peaks should occur (to create jagged activation)
+                if (latestPeak == 1) && ((i - latestPeak) > delta/2)
+                    latestPeak = i;
+                    peaks = [peaks; i];
+                else
+                    if (i - latestPeak) > delta
+                        latestPeak = i;
+                        peaks = [peaks; i];
+                    elseif (i - latestZero) > delta
+                        latestZero = i;
+                        zeros = [zeros; i];
+                    end  
+                end
+
+                % looking at the ascending curve to peak
+                if latestZero < latestPeak
+                    % check for empty eqns (check is not necessary for descending case
+                    % as it is assumed ascending comes first
+                    if isempty(eqns)
+                        coeffs = polyfit([simulatedTA(latestZero,1) simulatedTA(latestPeak,1)], [simulatedTA(latestZero,2) simulatedTA(latestPeak,2)], 1);
+                        eqns = [eqns; latestZero latestPeak coeffs(1) coeffs(2)];
+                    % checking if the coeffs have already been found for these
+                    % particular points
+                    elseif ~any(eqns(:,1) == latestZero)
+                        coeffs = polyfit([simulatedTA(latestZero,1) simulatedTA(latestPeak,1)], [0 simulatedTA(latestPeak,2)], 1);
+                        eqns = [eqns; latestZero latestPeak coeffs(1) coeffs(2)];
+                    end
+                % looking at the descending curve from peak 
+                elseif latestZero > latestPeak
+                    if ~any(eqns(:,1) == latestPeak)
+                        coeffs = polyfit([simulatedTA(latestPeak,1) simulatedTA(latestZero,1)], [simulatedTA(latestPeak,2) 0], 1);
+                        eqns = [eqns; latestPeak latestZero coeffs(1) coeffs(2)];
+                    end
+                end
+            end
+
+            % finded modelled activation values
+            activations = [];
+            for i = 1:size(simulatedTA,1)
+                if any(zeros(:) == i)
+                    % if labelled as a zero, input into the activation matrix with
+                    % activation of 0
+                    activations = [activations; simulatedTA(i,1) 0];
+                elseif any(peaks(:) == i)
+                    % if labelled as a peak, input into the activation matrix with
+                    % sample activation 
+                    activations = [activations; simulatedTA(i,1) simulatedTA(i,2)];
+                else
+                    % else, based on the location of the point, use the coefficients found 
+                    % to find the corresponding activation
+                    % find new value based on linearity between adjacent zero and peak
+                    for j = 1:size(eqns,1)
+                        if  i < eqns(j,2) && i > eqns(j,1)
+                            a = polyval([eqns(j,3) eqns(j,4)],simulatedTA(i,1));
+                            activations = [activations; simulatedTA(i,1) a];
+                        end
+                    end
+                end
+            end
+            
+            SP.modelledTA = activations;
+        end 
+        
+        
+        % will be loading data = csvread('S_STIM.csv');
+        %20-50Hz
+        function SP = getModelledTA(SP, data, frequency)
+            regressions = getActivationRegression(data, SP.gaitInterval);
+            simulatedS = getSimulatedActivations(regressions, SP.gaitInterval, SP.swingInterval);
+            
+            % using frequency to find how many points exist between zeros and peaks
+            delta = (SP.pointNum/SP.gaitInterval)/frequency;
+            % identifying where peaks and zeros occur, as well as identifying the linear
+            % relation between adjacents
+            latestPeak = 1; latestZero = 1;
+            peaks = []; zeros = [1];
+            eqns = []; % store info in format: start_index end_index coeff(1) coeff(2)
+            for i = 1:size(simulatedS,1)    
+                % identify where and when peaks should occur (to create jagged activation)
+                if (latestPeak == 1) && ((i - latestPeak) > delta/2)
+                    latestPeak = i;
+                    peaks = [peaks; i];
+                else
+                    if (i - latestPeak) > delta
+                        latestPeak = i;
+                        peaks = [peaks; i];
+                    elseif (i - latestZero) > delta
+                        latestZero = i;
+                        zeros = [zeros; i];
+                    end  
+                end
+
+                % looking at the ascending curve to peak
+                if latestZero < latestPeak
+                    % check for empty eqns (check is not necessary for descending case
+                    % as it is assumed ascending comes first
+                    if isempty(eqns)
+                        coeffs = polyfit([simulatedS(latestZero,1) simulatedS(latestPeak,1)], [simulatedS(latestZero,2) simulatedS(latestPeak,2)], 1);
+                        eqns = [eqns; latestZero latestPeak coeffs(1) coeffs(2)];
+                    % checking if the coeffs have already been found for these
+                    % particular points
+                    elseif ~any(eqns(:,1) == latestZero)
+                        coeffs = polyfit([simulatedS(latestZero,1) simulatedS(latestPeak,1)], [0 simulatedS(latestPeak,2)], 1);
+                        eqns = [eqns; latestZero latestPeak coeffs(1) coeffs(2)];
+                    end
+                % looking at the descending curve from peak 
+                elseif latestZero > latestPeak
+                    if ~any(eqns(:,1) == latestPeak)
+                        coeffs = polyfit([simulatedS(latestPeak,1) simulatedS(latestZero,1)], [simulatedS(latestPeak,2) 0], 1);
+                        eqns = [eqns; latestPeak latestZero coeffs(1) coeffs(2)];
+                    end
+                end
+            end
+
+            % finded modelled activation values
+            activations = [];
+            for i = 1:size(simulatedS,1)
+                if any(zeros(:) == i)
+                    % if labelled as a zero, input into the activation matrix with
+                    % activation of 0
+                    activations = [activations; simulatedS(i,1) 0];
+                elseif any(peaks(:) == i)
+                    % if labelled as a peak, input into the activation matrix with
+                    % sample activation 
+                    activations = [activations; simulatedS(i,1) simulatedS(i,2)];
+                else
+                    % else, based on the location of the point, use the coefficients found 
+                    % to find the corresponding activation
+                    % find new value based on linearity between adjacent zero and peak
+                    for j = 1:size(eqns,1)
+                        if  i < eqns(j,2) && i > eqns(j,1)
+                            a = polyval([eqns(j,3) eqns(j,4)],simulatedS(i,1));
+                            activations = [activations; simulatedS(i,1) a];
+                        end
+                    end
+                end
+            end
+            
+            SP.modelledS = activations;
         end 
         
         function aS = getActivationS(theta)
